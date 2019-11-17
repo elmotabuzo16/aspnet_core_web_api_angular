6acf8b7fdd2818670885b557bf438bb1785b936c
elmotabuzo16 <elmotabuzojr.ust@gmail.com>
2019-11-17 13:02:13 +0800

    Part 1 - Retrieving data from Database

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +12,31 @@ namespace DatingApp.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public WeatherForecastController(DataContext context)
+        {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+
+            return Ok(values);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetValues(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+            return Ok(value);
+        }
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
